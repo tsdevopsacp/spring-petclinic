8fe97ad41602c380cfd2e0b12818efbba7156ac0
tsdevopsacp <111490717+tsdevopsacp@users.noreply.github.com>
2022-12-23 17:36:48 +0530

    Updated package name with unittests

--- src/test/java/org/springframework/samples/petclinic/unittests/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/unittests/model/ValidatorTests.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.samples.petclinic.model;
+package org.springframework.samples.petclinic.unittests.model;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
