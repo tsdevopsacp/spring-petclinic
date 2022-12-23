b02d5efc1e61c1f227c067124777faeb4ba9e51b
tsdevopsacp <111490717+tsdevopsacp@users.noreply.github.com>
2022-12-23 14:38:27 +0530

    Updated and renamed PetClinicIntegrationTests.java to SeleniumIntegrationTest.java

---
+++ src/test/java/org/springframework/samples/petclinic/SeleniumIntegrationTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.samples.petclinic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PetClinicIntegrationTests {
+
+        @LocalServerPort
+        int port;
+
+        @Test
+        public void testTitle() {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                WebDriver driver = new ChromeDriver(options);
+                driver.get("http://localhost:" + port);
+
+                String title = driver.getTitle();
+                assertEquals("PetClinic :: a Spring Framework demonstration", title);
+
+                driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+                driver.quit();
+        }
+
+}
